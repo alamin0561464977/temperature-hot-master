6ffb0674ab7392b560aa5f7e501176382bbeff44
alamin0561464977 <amin23435678901315@gmail.com>
2022-03-02 15:33:44 +0300

    input Filed OK

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-console.log('alamin')
+const Key = `94c59728ba02f2225d911846992f6b5c`;
+const weatherDataLoad = () => {
+    const inputFiled = document.getElementById('input-filed').value;
+    console.log(inputFiled)
+}
